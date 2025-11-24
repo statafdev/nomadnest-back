b2b39ffe46ed59ab1638722c712e0b44142f50a4
Lan-c <stataf.dev@gmail.com>
2025-11-24 17:22:19 +0100

    vercel merge

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -79,6 +79,7 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
+    console.log(req.body);
 
     // Validation
     if (!email || !password) {
